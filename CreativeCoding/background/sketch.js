d26d3c30103e5f2e8237b40e2908f3332266f889
winstonmock <113491680+winstonmock@users.noreply.github.com>
2023-01-12 12:50:49 -0500

    s

--- CreativeCoding/background/sketch.js
+++ CreativeCoding/background/sketch.js
@@ -1,8 +1,31 @@
+let gif;
+
+ function preload(){
+
+   img = loadImage ('Assets/guy1.psd');
+ }
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background (255,255,0);
+  createCanvas(windowWidth,windowWidth);
+  background(255,255,0);
 }
 
 function draw() {
+  fill(0,255,255);
+  strokeWeight(1);
+  var num = 20;
+  var sideLen = windowWidth/num;
 
+  translate(-150,-150);//give design bleed effect
+
+for (var y = 0; y < 2 * windowWidth;y += sideLen){
+  for(var x = 0; x< 2 * windowWidth;x += sideLen){
+      image (img,x,y,sideLen,sideLen);
+  }
+}
+
+}
+
+function windowResized (){// This resize and refreshes the design with broswer
+  resizeCanvas (windowWidth,windowHeight);
 }
